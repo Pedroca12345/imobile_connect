366a1b555de546e7a3b8c7a996e5bd215ec106c1
Pedroca12345 <pedrohenriqueanacleto789@gmail.com>
2025-03-05 19:06:31 -0300

    Register button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,24 +2,16 @@ import './Header.css';
 import React from 'react';
 import { BlueLogo } from '../blue_logo/BlueLogo';
 import { LoginButton } from '../login_button/LoginButton';
+import { RegisterButton } from '../register_button/RegisterButton';
 
 export function Header() {
     return (
         <header className='CTA_header'>
-
             <div className="CTA_header_container">
-
                 <BlueLogo />
-
                 <div className="CTA_header_navigation">
                     <LoginButton />
-
-                    <div className="register_button">
-                        <a href="#">
-                            <button>Cadastrar-se</button>
-                        </a>
-                    </div>
-
+                    <RegisterButton />
                 </div>
             </div>
 
---
+++ src/components/register_button/RegisterButton.jsx
@@ -0,0 +1,11 @@
+import './RegisterButton.css';
+
+export function RegisterButton() {
+    return (
+        <div className="register_button">
+            <a href="#">
+                <button className='poppins_bold'>Cadastrar-se</button>
+            </a>
+        </div>
+    )
+}
