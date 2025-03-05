c1c242d9828591639b233b8b3d467cdd301f130b
Pedroca12345 <pedrohenriqueanacleto789@gmail.com>
2025-03-05 18:28:36 -0300

    alguns resets e import do header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import './App.css';
+import { Header } from './components/header/Header';
 
 function App() {
 
   return (
     <>
       <div className="app">
-        
+        <Header />
       </div>
     </>
   )
